bd2d7a52355189742b204e65daa25232a4ec8a49
angular8287 <angular8287@caelumBSB01-09>
2019-06-13 11:47:50 -0300

    import routermodule no shared comonents

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -3,13 +3,15 @@ import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { CmailFormGroupComponent } from '../modules/cmail-form-group/cmail-form-group.component';
 import { CmailFormFieldDirective } from '../modules/cmail-form-group/cmail-form-field.directive';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ // declara componentes e outros
     HeaderComponent,
   ],
   imports: [ // importa módulos
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [ //exporta componentes e outros
     HeaderComponent,
